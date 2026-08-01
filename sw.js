309d3b1bff3e1fdbd2340ff7308ef3dfdee545da
Tanovasea <ionpruteanu144@gmail.com>
2026-08-01 14:56:05 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
    la activare șterge tot ce e din versiunile trecute.
    ============================================================ */
 
-const VERSIUNE = 'logos-2026-08-01-q';   // ← schimbă asta la fiecare încărcare
+const VERSIUNE = 'logos-2026-08-01-r';   // ← schimbă asta la fiecare încărcare
 
 const FISIERE = [
   './',
